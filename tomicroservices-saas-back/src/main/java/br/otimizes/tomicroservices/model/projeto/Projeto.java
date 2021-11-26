e13be891c590b6508da9daac23e51a6816745702
Pedro Lobato <pedrolobato1305@gmail.com>
2021-11-25 23:20:00 -0300

    Implementação back - projetos

--- tomicroservices-saas-back/src/main/java/br/otimizes/tomicroservices/model/projeto/Projeto.java
+++ tomicroservices-saas-back/src/main/java/br/otimizes/tomicroservices/model/projeto/Projeto.java
@@ -10,20 +10,28 @@ import javax.persistence.OneToMany;
 
 import br.otimizes.tomicroservices.base.BaseEntity;
 import br.otimizes.tomicroservices.model.usuário.Usuario;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
 
 @Entity
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
+@Indexed
 public class Projeto extends BaseEntity {
+
+	@Field
 	private String nome;
+
+	@Field
 	private String identificador;
+
 	@ManyToOne
 	@JoinColumn(name = "proprietario_id")
 	private Usuario proprietario;
+
 	@OneToMany
 	@JoinColumn(name = "projeto_id")	
 	private List<Usuario> participantes = new ArrayList<>();
