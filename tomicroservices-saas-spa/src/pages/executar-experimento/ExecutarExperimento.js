5f598a76b6064ce92da36d15c472be4e5cfe79bb
arthur.zavadski <aczavads@gmail.com>
2021-11-27 00:10:52 -0300

    Adicionada capacidade de selecionar as métricas para executar o experimento

--- tomicroservices-saas-spa/src/pages/executar-experimento/ExecutarExperimento.js
+++ tomicroservices-saas-spa/src/pages/executar-experimento/ExecutarExperimento.js
@@ -6,7 +6,9 @@ import api from '../../services/api';
 
 const schema = yup.object({
     numberOfMicroservices: yup.number().positive().integer().required(),
-    executions: yup.number().positive().integer().required()
+    executions: yup.number().positive().integer().required(),
+    crossoverProbability: yup.number().min(0.0).max(1.0).required(),
+    crossoverFraction: yup.number().min(0.0).max(1.0).required(),
 }).required();
 
 
@@ -25,18 +27,26 @@ const ExecutarExperimento = () => {
         }
         setExecuting(true);
         try {
-            const postRequest = await api.post("/api/experimentos", { 
+            var request = {
                 numberOfMicroservices: data.numberOfMicroservices,
-                executions : data.executions,
-                crossoverProbability: 0.8,
-                crossoverFraction: 0.8,            
+                executions: data.executions,
+                crossoverProbability: data.crossoverProbability,
+                crossoverFraction: data.crossoverFraction,
                 dynamicLogFileId: "803177c9-cde0-469e-bd42-c5e6972bae05",
-                metrics : [	
-                    "Coupling", 
-                    "Cohesion",
-                    "##Reuse"
-                ]	            
-            });
+                metrics: [
+                ]
+            };
+            request.metrics = data.couplingMetric ? [...request.metrics, "Coupling"] :  request.metrics;
+            request.metrics = data.cohesionMetric ? [...request.metrics, "Cohesion"] :  request.metrics;
+            request.metrics = data.overheadMax ? [...request.metrics, "OverheadMax"] :  request.metrics;
+            request.metrics = data.functionality ? [...request.metrics, "Functionality"] :  request.metrics;
+            request.metrics = data.size ? [...request.metrics, "Size"] :  request.metrics;
+            request.metrics = data.reuse ? [...request.metrics, "Reuse"] :  request.metrics;
+            
+            console.log(data);
+            console.log(request);
+
+            const postRequest = await api.post("/api/experimentos", request);
             console.log(postRequest.data);
         } catch (error) {
             console.log(error);
@@ -48,23 +58,85 @@ const ExecutarExperimento = () => {
     return (
 
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div class="form-row">
-                <label for="inputNumberOfMicroservices">Número de microserviços</label>
+            <div className="form-row">
+                <label htmlFor="inputNumberOfMicroservices">Número de microserviços</label>
                 <div className="form-group col-5">
                     <input {...register("numberOfMicroservices")} id="inputNumberOfMicroservices" className={`form-control ${errors.numberOfMicroservices ? 'is-invalid' : ''}`} />
                     <div className="invalid-feedback">{errors.numberOfMicroservices?.message}</div>
                 </div>
             </div>
-            <div class="form-row">
-                <label for="inputExecutions">Número de execuções</label>
+            <div className="form-row">
+                <label htmlFor="inputExecutions">Número de execuções</label>
                 <div className="form-group col-5">
                     <input {...register("executions")} id="inputExecutions" className={`form-control ${errors.executions ? 'is-invalid' : ''}`} />
                     <div className="invalid-feedback">{errors.executions?.message}</div>
                 </div>
             </div>
+            <div className="form-row">
+                <label htmlFor="inputCrossoverProbability">Probabilidade de crossover (de 0.0 a 1.0)</label>
+                <div className="form-group col-5">
+                    <input {...register("crossoverProbability")} id="inputCrossoverProbability" className={`form-control ${errors.crossoverProbability ? 'is-invalid' : ''}`} />
+                    <div className="invalid-feedback">{errors.crossoverProbability?.message}</div>
+                </div>
+            </div>
+            <div className="form-row">
+                <label htmlFor="inputCrossoverFraction">Fração de crossover (de 0.0 a 1.0)</label>
+                <div className="form-group col-5">
+                    <input {...register("crossoverFraction")} id="inputCrossoverFraction" className={`form-control ${errors.crossoverFraction ? 'is-invalid' : ''}`} />
+                    <div className="invalid-feedback">{errors.crossoverFraction?.message}</div>
+                </div>
+            </div>
+            <div className="form-row">
+                <div class="form-check">
+                    <input {...register("couplingMetric")} className="form-check-input" type="checkbox" value="" id="checkboxCoupling"></input>
+                    <label className ="form-check-label" for="checkboxCoupling">
+                    Coupling
+                    </label>
+                </div>
+            </div>
+            <div className="form-row">
+                <div class="form-check">
+                    <input {...register("cohesionMetric")} className="form-check-input" type="checkbox" value="" id="checkboxCohesion"></input>
+                    <label className ="form-check-label" for="checkboxCohesion">
+                    Cohesion
+                    </label>
+                </div>
+            </div>
+            <div className="form-row">
+                <div class="form-check">
+                    <input {...register("overheadMax")} className="form-check-input" type="checkbox" value="" id="checkboxOverheadMax"></input>
+                    <label className ="form-check-label" for="checkboxOverheadMax">
+                    OverheadMax
+                    </label>
+                </div>
+            </div>
+            <div className="form-row">
+                <div class="form-check">
+                    <input {...register("functionality")} className="form-check-input" type="checkbox" value="" id="checkboxFunctionality"></input>
+                    <label className ="form-check-label" for="checkboxFunctionality">
+                    Functionality
+                    </label>
+                </div>
+            </div>
+            <div className="form-row">
+                <div class="form-check">
+                    <input {...register("size")} className="form-check-input" type="checkbox" value="" id="checkboxSize"></input>
+                    <label className ="form-check-label" for="checkboxSize">
+                    Size
+                    </label>
+                </div>
+            </div>
+            <div className="form-row">
+                <div class="form-check">
+                    <input {...register("reuse")} className="form-check-input" type="checkbox" value="" id="checkboxReuse"></input>
+                    <label className ="form-check-label" for="checkboxReuse">
+                    Reuse
+                    </label>
+                </div>
+            </div>
             <br />
-            <div class="form-row">
-                <button class="btn btn-primary" type="submit" onClick={handleSubmit(executarExperimento)}>Executar Experimento</button>
+            <div className="form-row">
+                <button className="btn btn-primary" type="submit" onClick={handleSubmit(executarExperimento)}>Executar Experimento</button>
             </div>
         </form>
     );
