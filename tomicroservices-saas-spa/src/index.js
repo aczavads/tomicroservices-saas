55ba34aa40d19fceb6703b822ceb85268255196b
arthur.zavadski <aczavads@gmail.com>
2021-11-27 22:14:20 -0300

    Visualização do experimento implementada

--- tomicroservices-saas-spa/src/index.js
+++ tomicroservices-saas-spa/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';  
 
 ReactDOM.render(
   <React.StrictMode>
--- tomicroservices-saas-spa/src/pages/executar-experimento/ExecutarExperimento.js
+++ tomicroservices-saas-spa/src/pages/executar-experimento/ExecutarExperimento.js
@@ -16,6 +16,8 @@ const ExecutarExperimento = () => {
     const [executing, setExecuting] = useState(false);
     const [fileChanged, setFileChanged] = useState(false);
     const [newFile, setNewFile] = useState(null);
+    const [dynamicLogFileId, setDynamicLogFileId] = useState(null);
+    const [results, setResults] = useState([]);
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema)
     });
@@ -33,7 +35,7 @@ const ExecutarExperimento = () => {
 
     const uploadFile = async (event) => {
         if (executing) {
-            alert("Opa, espera aí!");
+            alert("Já existe uma execução em andamento!");
         }
         setExecuting(true);
         try {
@@ -46,6 +48,7 @@ const ExecutarExperimento = () => {
             console.log(newFile);
             const postRequest = await api.post("/api/experimentos/upload/dynamic-log-file", formData);
             console.log(postRequest.data);
+            setDynamicLogFileId(postRequest.data);
         } catch (error) {
             console.log(error);
         }
@@ -55,16 +58,21 @@ const ExecutarExperimento = () => {
     const executarExperimento = async (data) => {
         //alert("Opa, executando!!!");
         if (executing) {
-            alert("Opa, espera aí!");
+            alert("Já existe uma execução em andamento!");
+            return;
         }
-        setExecuting(true);
+        if (dynamicLogFileId === undefined || dynamicLogFileId === null) {
+            alert("É necessário fazer o upload de um arquivo de log dinâmico para iniciar uma execução!")
+            return;
+        }
         try {
             var request = {
                 numberOfMicroservices: data.numberOfMicroservices,
                 executions: data.executions,
                 crossoverProbability: data.crossoverProbability,
                 crossoverFraction: data.crossoverFraction,
-                dynamicLogFileId: "803177c9-cde0-469e-bd42-c5e6972bae05",
+                dynamicLogFileId: dynamicLogFileId,
+                //dynamicLogFileId: "803177c9-cde0-469e-bd42-c5e6972bae05",
                 metrics: [
                 ]
             };
@@ -75,11 +83,18 @@ const ExecutarExperimento = () => {
             request.metrics = data.size ? [...request.metrics, "Size"] : request.metrics;
             request.metrics = data.reuse ? [...request.metrics, "Reuse"] : request.metrics;
 
+            if (request.metrics.length === 0) {
+                alert("É necessário selecionar pelo menos uma métrica para iniciar uma execução!")
+                return;
+
+            }
+            setExecuting(true);
             console.log(data);
             console.log(request);
 
             const postRequest = await api.post("/api/experimentos", request);
-            console.log(postRequest.data);
+            console.log(postRequest);
+            setResults(postRequest.data);
         } catch (error) {
             console.log(error);
         }
@@ -91,7 +106,7 @@ const ExecutarExperimento = () => {
 
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-row">
-                <label htmlFor="formFileSm" class="form-label">Arquivo de log dinâmico</label>
+                <label htmlFor="formFileSm" className="form-label">Arquivo de log dinâmico</label>
                 <div className="form-group col-5">
                     <input className="form-control form-control-sm" id="formFileSm" type="file" onChange={onFileChange} />
                     <button className={`btn btn-primary ${fileChanged ? '' : 'disabled'}`} type="button" onClick={onFileUpload}>Carregar arquivo</button>
@@ -127,49 +142,49 @@ const ExecutarExperimento = () => {
                 </div>
             </div>
             <div className="form-row">
-                <div class="form-check">
+                <div className="form-check">
                     <input {...register("couplingMetric")} className="form-check-input" type="checkbox" value="" id="checkboxCoupling"></input>
-                    <label className="form-check-label" for="checkboxCoupling">
+                    <label className="form-check-label" htmlFor="checkboxCoupling">
                         Coupling
                     </label>
                 </div>
             </div>
             <div className="form-row">
-                <div class="form-check">
+                <div className="form-check">
                     <input {...register("cohesionMetric")} className="form-check-input" type="checkbox" value="" id="checkboxCohesion"></input>
-                    <label className="form-check-label" for="checkboxCohesion">
+                    <label className="form-check-label" htmlFor="checkboxCohesion">
                         Cohesion
                     </label>
                 </div>
             </div>
             <div className="form-row">
-                <div class="form-check">
+                <div className="form-check">
                     <input {...register("overheadMax")} className="form-check-input" type="checkbox" value="" id="checkboxOverheadMax"></input>
-                    <label className="form-check-label" for="checkboxOverheadMax">
+                    <label className="form-check-label" htmlFor="checkboxOverheadMax">
                         OverheadMax
                     </label>
                 </div>
             </div>
             <div className="form-row">
-                <div class="form-check">
+                <div className="form-check">
                     <input {...register("functionality")} className="form-check-input" type="checkbox" value="" id="checkboxFunctionality"></input>
-                    <label className="form-check-label" for="checkboxFunctionality">
+                    <label className="form-check-label" htmlFor="checkboxFunctionality">
                         Functionality
                     </label>
                 </div>
             </div>
             <div className="form-row">
-                <div class="form-check">
+                <div className="form-check">
                     <input {...register("size")} className="form-check-input" type="checkbox" value="" id="checkboxSize"></input>
-                    <label className="form-check-label" for="checkboxSize">
+                    <label className="form-check-label" htmlFor="checkboxSize">
                         Size
                     </label>
                 </div>
             </div>
             <div className="form-row">
-                <div class="form-check">
+                <div className="form-check">
                     <input {...register("reuse")} className="form-check-input" type="checkbox" value="" id="checkboxReuse"></input>
-                    <label className="form-check-label" for="checkboxReuse">
+                    <label className="form-check-label" htmlFor="checkboxReuse">
                         Reuse
                     </label>
                 </div>
@@ -178,8 +193,42 @@ const ExecutarExperimento = () => {
             <div className="form-row">
                 <button className="btn btn-primary" type="submit" onClick={handleSubmit(executarExperimento)}>Executar Experimento</button>
             </div>
+            <br />
+            <hr />
+            <AccordionResults results={results}></AccordionResults>
         </form>
     );
+}
+
+const RenderResults = (props) => {
+    const resultsRendered = props.results.map((result, index) => {
+        return (
+            <div className="accordion-item" key={index}>
+                <h2 className="accordion-header" id={`heading${index}`}>
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="true" aria-controls={`collapse${index}`}>
+                        {`Execução ${index}`}
+                    </button>
+                </h2>
+                <div id={`collapse${index}`} className="accordion-collapse collapse show" aria-labelledby={`heading${index}`} data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                        {result.map((value, valueIndex) => <p key={valueIndex}>{value}</p>)}
+                    </div>
+                </div>
+            </div>
+        );
+    });
+    return (<React.Fragment>{resultsRendered}</React.Fragment>);
+}
+
+const AccordionResults = (props) => {
+    if (props.results.length === 0) {
+        return (<React.Fragment></React.Fragment>);
+    }
+    return (
+        <div className="accordion" id="accordionExample">
+            <RenderResults results={props.results}></RenderResults>
+        </div>
+    )
 }
 
 
